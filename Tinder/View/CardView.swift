797f1e6386cf2800bc9dbf00abc1ca0c3cce91d3
Edwin Cardenas <ecardenas@lwolf.com>
2025-10-06 18:34:27 -0500

    feat: add info label and button to card view

--- Tinder/View/CardView.swift
+++ Tinder/View/CardView.swift
@@ -21,6 +21,46 @@ class CardView: UIView {
         return _imageView
     }()
 
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+
+        let attributedText = NSMutableAttributedString(
+            string: "Jane Doe",
+            attributes: [
+                .font: UIFont.systemFont(ofSize: 32, weight: .heavy),
+                .foregroundColor: UIColor.white,
+            ]
+        )
+
+        attributedText.append(
+            NSAttributedString(
+                string: "  18",
+                attributes: [
+                    .font: UIFont.systemFont(ofSize: 24),
+                    .foregroundColor: UIColor.white,
+                ]
+            )
+        )
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        label.attributedText = attributedText
+
+        return label
+    }()
+
+    private lazy var infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(resource: .infoIcon).withRenderingMode(
+            .alwaysOriginal
+        )
+
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(image, for: .normal)
+
+        return button
+    }()
+
     // MARK: - Initializers
 
     override init(frame: CGRect) {
@@ -43,6 +83,8 @@ extension CardView {
         translatesAutoresizingMaskIntoConstraints = false
 
         addSubview(imageView)
+        addSubview(infoLabel)
+        addSubview(infoButton)
 
         // imageView
         NSLayoutConstraint.activate([
@@ -50,6 +92,35 @@ extension CardView {
             imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+
+        // infoLabel
+        NSLayoutConstraint.activate([
+            infoLabel.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: 16
+            ),
+            infoLabel.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: -16
+            ),
+            infoLabel.bottomAnchor.constraint(
+                equalTo: bottomAnchor,
+                constant: -16
+            ),
+        ])
+
+        // infoButton
+        NSLayoutConstraint.activate([
+            infoButton.heightAnchor.constraint(equalToConstant: 40),
+            infoButton.widthAnchor.constraint(equalTo: infoButton.heightAnchor),
+            infoButton.centerYAnchor.constraint(
+                equalTo: infoLabel.centerYAnchor
+            ),
+            infoButton.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: -16
+            ),
         ])
     }
 
